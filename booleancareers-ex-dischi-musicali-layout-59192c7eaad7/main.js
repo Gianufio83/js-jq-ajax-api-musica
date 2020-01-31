144995861e5ff7737fe41fd84b84906b9a900290
Gianufio83 <fiore.gianuario@gmail.com>
2020-01-31 15:14:55 +0100

    prima parte esercizio completata

--- booleancareers-ex-dischi-musicali-layout-59192c7eaad7/main.js
+++ booleancareers-ex-dischi-musicali-layout-59192c7eaad7/main.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-	//Code
+
+  $.ajax(
+    {
+      'url' : 'https://flynn.boolean.careers/exercises/api/array/music',
+      'method' : 'GET',
+      'success': function(data) {
+        console.log(data);
+        printData(data.response);
+
+      },
+      'error' : function(request, state, errors) {
+        alert('Errore' + errors);
+      }
+    }
+  );
+    function printData(cds) {
+     for (var i = 0; i < cds.length; i++) {
+        var cd = cds[i];
+        var source = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+        var html = template(cd);
+        $('.cds-container').append(html);
+     }
+    }
 });
